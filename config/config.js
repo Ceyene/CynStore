826bf63a6cb2b2a863c2fffff8db12e029d7d331
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-17 22:53:56 -0300

    Adding .env variables configuration

---
+++ config/config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+//dotenv will save all our .env values in the following env variables
+const config = {
+  env: process.env.NODE_ENV || 'dev',
+  port: process.env.PORT || 3000,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
+};
+
+module.exports = { config };
